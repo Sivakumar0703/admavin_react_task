04b5a5a1792536acce18b265d4690321cc686a7a
sivakumar0703 <ssivakumar358@gmail.com>
2024-11-05 15:18:50 +0530

    task-1 completed

--- src/logics/bucketLogics.js
+++ src/logics/bucketLogics.js
@@ -1,44 +1,91 @@
 
 // selecting and de-selecting the items
-export function selectionAndDeselection(e,selectedItemsRef,setSelectedItems,removeEventListners) {
+export function selectionAndDeselection(e,selectedItemsRef,setSelectedItems,destinationBucket,handleDisable) {
     const selectedItemValue = e.target.innerText;
-    const checkAvailability = selectedItemsRef.current.some(item => item.innerText == selectedItemValue);
-    if(e.target.tagName == "P"){
-        
+    const checkAvailability = selectedItemsRef.current.some(item => item == selectedItemValue);
+    if(e.target.tagName == "P"){       
         if(checkAvailability){
-            console.log("already")
             e.target.style.opacity = "1";
-            const newlySelectedValue = selectedItemsRef.current.filter(item => item.innerText != selectedItemValue)
-            setSelectedItems(newlySelectedValue)
-            console.log(newlySelectedValue.length)
+            const newlySelectedValue = selectedItemsRef.current.filter(item => item != selectedItemValue);
+            setSelectedItems(newlySelectedValue);
             if(!newlySelectedValue.length){
-                removeEventListners();
+                destinationBucket.removeEventListener('mouseenter' , handleDisable);
+                // enableSelectionArea(destinationBucket);
+                disableOrEnableSelection(destinationBucket,"enable");
             }
         } else {
-            console.log("adding")
-            e.target.style.opacity = "0.5"
-            setSelectedItems(prev => [...prev , e.target])
+            e.target.style.opacity = "0.5";
+            setSelectedItems(prev => [...prev , selectedItemValue]);
         }
-
     }
 }
 
 // disable cursor for selecting items in destination bucket
-export function disableSelectionArea(bucket){
-    bucket.style.cursor = "not-allowed";
-    const pTags = bucket.getElementsByTagName('p');
-    for(let i=0; i<pTags.length; i++){
-        pTags[i].style.cursor = "not-allowed";
-    }
-}
+// export function disableSelectionArea(bucket){
+//     bucket.style.cursor = "not-allowed";
+//     const pTags = bucket.getElementsByTagName('p');
+//     for(let i=0; i<pTags.length; i++){
+//         pTags[i].style.cursor = "not-allowed";
+//     }
+// }
 
 // enable cursor for selecting items
-export function enableSelectionArea(bucket){
-    bucket.style.cursor = "default";
+// export function enableSelectionArea(bucket){
+//     bucket.style.cursor = "default";
+//     const pTags = bucket.getElementsByTagName('p');
+//     for(let i=0; i<pTags.length; i++){
+//         pTags[i].style.cursor = "pointer";
+//     }
+// }
+
+// operation = enable | operation = disable
+export function disableOrEnableSelection(bucket,operation){
+    bucket.style.cursor = operation === "disable" ? "not-allowed" : "default" ;
     const pTags = bucket.getElementsByTagName('p');
     for(let i=0; i<pTags.length; i++){
-        pTags[i].style.cursor = "pointer";
+        pTags[i].style.cursor = operation === "disable" ? "not-allowed" : "pointer" ;
     }
 }
 
+// transfer single/multiple items 
+export function transferItems(setSelectedItems,selectedItemsRef,setBucket1,setBucket2,bucket1,bucket2,bucketContainer,handleDisable){
+    let selectedItems = selectedItemsRef.current;
+    if(bucketContainer.transferringBucket.id === "bucket-1"){
+        setBucket1(removeSentItemsFromBucket(selectedItems , bucket1));
+        setBucket2(prev => [...prev , ...selectedItems]);
+    } else {
+        setBucket2(removeSentItemsFromBucket(selectedItems , bucket2));
+        setBucket1(prev => [...prev , ...selectedItems]);
+    }
+    bucketContainer.destinationBucket.removeEventListener('mouseenter' , handleDisable);
+    // enableSelectionArea(bucketContainer.destinationBucket);
+    disableOrEnableSelection(bucketContainer.destinationBucket,"enable");
+    setSelectedItems([]);
+}
+
+// removing the transferring items from the transferring bucket
+function removeSentItemsFromBucket(selectedItems , storedItems){
+    let remainingItems;
+    if(selectedItems.length > 1){
+        remainingItems = storedItems.filter(item => !selectedItems.includes(item));
+    } else {
+        remainingItems = storedItems.filter(item => item !== selectedItems[0]);   
+    }
+    return remainingItems
+}
+
+// delete single/multiple items
+export function deleteItems(bucketContainer,selectedItemsRef,bucket1,bucket2,setBucket1,setBucket2,setSelectedItems,handleDisable){
+    if(bucketContainer.transferringBucket.id === "bucket-1"){
+        const updatedItemsList = bucket1.filter(item => !selectedItemsRef.current.includes(item));
+        setBucket1(updatedItemsList);
+    } else {
+        const updatedItemsList = bucket2.filter(item => !selectedItemsRef.current.includes(item));
+        setBucket2(updatedItemsList);
+    }
+    bucketContainer.destinationBucket.removeEventListener('mouseenter' , handleDisable);
+    // enableSelectionArea(bucketContainer.destinationBucket);
+    disableOrEnableSelection(bucketContainer.destinationBucket,"enable");
+    setSelectedItems([]);
+}
 
--- src/routes/Buckets.jsx
+++ src/routes/Buckets.jsx
@@ -1,61 +1,63 @@
 import { useEffect, useRef, useState } from 'react'
 import Button from '../components/Button'
-import { disableSelectionArea, enableSelectionArea, selectionAndDeselection } from '../logics/bucketLogics';
+import { deleteItems, disableOrEnableSelection, selectionAndDeselection, transferItems } from '../logics/bucketLogics';
 
 const Buckets = () => {
 
     const [selectedItems , setSelectedItems] = useState([]);
     const selectedItemsRef = useRef(null);
+    const [bucketContainer , setBucketContainer] = useState({transferringBucket:{},destinationBucket:{}}); // contains transferring and destionation bucket div
     selectedItemsRef.current = selectedItems;
-    console.log("from outside",selectedItems , selectedItems.length > 1)
+    const [bucket1 , setBucket1] = useState(["Item-1","Item-2","Item-3","Item-5"]);
+    const [bucket2 , setBucket2] = useState(["Item-4","Item-6"]);
 
+    const handleDisable = (destinationBucket) => disableOrEnableSelection(destinationBucket,"disable");
 
 
 
     useEffect(() => {
 
-        const bucket1 = document.getElementById('bucket-1');
-        const bucket2 = document.getElementById('bucket-2');
+        const bucket1Div = document.getElementById('bucket-1');
+        const bucket2Div = document.getElementById('bucket-2');
         let itemTransferringBucket;
         let destinationBucket;
 
-        const handleSelection = (e) => selectionAndDeselection(e,selectedItemsRef,setSelectedItems,removeEventListners);
-        const handleDisable = () => disableSelectionArea(destinationBucket);
-        const handleEnable = () => enableSelectionArea(destinationBucket);
+        const handleSelection = (e) => selectionAndDeselection(e,selectedItemsRef,setSelectedItems,destinationBucket,handleDisable);
 
         function setupEventListners(){
             if(itemTransferringBucket && destinationBucket){
-                // itemTransferringBucket.addEventListener('click' , handleSelection);
-                destinationBucket.addEventListener('mouseenter' , handleDisable);
-                destinationBucket.addEventListener('mouseleave' , handleEnable);
+                destinationBucket.addEventListener('mouseenter' , handleDisable(destinationBucket));
             }
         }
 
-        function removeEventListners(){
-            if(itemTransferringBucket && destinationBucket){
-                itemTransferringBucket.removeEventListener('click' , handleSelection);
-                destinationBucket.removeEventListener('mouseenter' , handleDisable);
-                destinationBucket.removeEventListener('mouseleave' , handleEnable);
-            }
-        }
-
-        function handleBucketClick(clickedBucket,e){
-            itemTransferringBucket = clickedBucket === "bucket1" ? bucket1 : bucket2;
-            destinationBucket = clickedBucket === "bucket1" ? bucket2 : bucket1;
-            // handleSelection(e);
+        function handleBucketClick(clickedBucket){
+            itemTransferringBucket = clickedBucket === "bucket1" ? bucket1Div : bucket2Div;
+            destinationBucket = clickedBucket === "bucket1" ? bucket2Div : bucket1Div;
             setupEventListners();
+            if(clickedBucket === "bucket1"){
+                setBucketContainer({transferringBucket:bucket1Div,destinationBucket:bucket2Div})
+            } else {
+                setBucketContainer({transferringBucket:bucket2Div,destinationBucket:bucket1Div})
+            }
         }
 
-        bucket1.addEventListener('click' , (e) => {
+        bucket1Div.addEventListener('click' , (e) => {
             handleBucketClick("bucket1",e)
             itemTransferringBucket.addEventListener('click' , handleSelection(e));
         })
 
-        bucket2.addEventListener('click' , (e) => {
+        bucket2Div.addEventListener('click' , (e) => {
             handleBucketClick("bucket2",e)
             itemTransferringBucket.addEventListener('click' , handleSelection(e));
         })
-        // bucket2.addEventListener('click' , (e) => handleBucketClick("bucket2",e) )
+
+        // on component unmount
+        function removeEventListners(){
+            if(itemTransferringBucket && destinationBucket){
+                itemTransferringBucket.removeEventListener('click' , handleSelection);
+                destinationBucket.removeEventListener('mouseenter' , handleDisable);
+            }
+        }
   
         return () => removeEventListners();
 
@@ -68,23 +70,23 @@ const Buckets = () => {
 
             <div id='bucket-1' className='bucket'>
                 <h2 className='title'>Bucket-1</h2>
-                <p className='items'>Item-1</p>
-                <p className='items'>Item-2</p>
-                <p className='items'>Item-3</p>
-                <p className='items'>Item-5</p>
+                {
+                    bucket1?.map((item) => <p key={item} className='items'>{item}</p> )
+                }
             </div>
 
             <div id='options'>
-                <Button name={"Add"} color="success" isDisabled={selectedItems.length === 1 ? false : true}  />
-                <Button name={"Remove"} color="danger" isDisabled={selectedItems.length === 1 ? false : true}  />
-                <Button name={"Add All"} color="success" isDisabled={selectedItems.length > 1 ? false : true}  />
-                <Button name={"Remove All"} color="danger" isDisabled={selectedItems.length > 1 ? false : true}  />
+                <Button name={"Add"} color="success" fn={() => transferItems(setSelectedItems,selectedItemsRef,setBucket1,setBucket2,bucket1,bucket2,bucketContainer,handleDisable)} isDisabled={selectedItems.length === 1 ? false : true}  />
+                <Button name={"Remove"} color="danger" fn={() => deleteItems(bucketContainer,selectedItemsRef,bucket1,bucket2,setBucket1,setBucket2,setSelectedItems,handleDisable)}  isDisabled={selectedItems.length === 1 ? false : true}  />
+                <Button name={"Add All"} color="success" fn={() => transferItems(setSelectedItems,selectedItemsRef,setBucket1,setBucket2,bucket1,bucket2,bucketContainer,handleDisable)} isDisabled={selectedItems.length > 1 ? false : true}  />
+                <Button name={"Remove All"} color="danger" fn={() => deleteItems(bucketContainer,selectedItemsRef,bucket1,bucket2,setBucket1,setBucket2,setSelectedItems,handleDisable)}  isDisabled={selectedItems.length > 1 ? false : true}  />
             </div>
 
             <div id='bucket-2' className='bucket'>
             <h2 className='title'>Bucket-2</h2>
-            <p className='items'>Item-4</p>
-            <p className='items'>Item-6</p>
+            {
+                bucket2?.map((item) => <p key={item} className='items'>{item}</p> )
+            }
             </div>
 
         </div>
